4ad616dccb261cb8cd38424930d4ca155cdec725
Cristobal A Alvarado <70360613+caalvarado6@users.noreply.github.com>
2020-10-06 23:42:02 -0600

    Update p1_avg.h

--- p1_avg.h
+++ p1_avg.h
@@ -2,14 +2,18 @@
 
 void main_avg(void) {
   int n;
+  printf("Enter 3 or 4:\n");//I added this instruction for the user to know what to do
   scanf("%d", &n);
+
   if(n == 3) {
-    int x1, x2, x3;
-    scanf("%d %d %d", &x1, &x2, &x3);
-    printf("%d\n", (x1+x2+x3) / 3);
+    double x1, x2, x3, Xt;
+    printf("Enter 3 numbers:\n");//This tells the user how many numbers to enter
+    scanf("%lf %lf %lf", &x1, &x2, &x3);
+    printf("The product is: %.2lf\n", (x1+x2+x3) / 3);;
   } else {
-    int x1, x2, x3, x4;
-    scanf("%d %d %d %d", &x1, &x2, &x3, &x4);
-    printf("%d\n", (x1+x2+x3+x4) / 3);
+    double x1, x2, x3, x4;
+    printf("Enter 4 numbers:\n");// tells the user to input 4 numbers
+    scanf("%lf %lf %lf %lf", &x1, &x2, &x3, &x4);
+    printf("The product is: %.2lf\n", (x1+x2+x3+x4) / 4);// at the end I just modify it just alittle to print to the user the product of the numbers
   }
 }
